77a495b5b284674b5b37684c268ff0babfc0df85
Priyanka Shelke <priyanka.shelke@uopca.unipune.ac.in>
2026-06-03 18:27:08 +0530

    Update command processor loop

--- 18_command_processor_loop.py
+++ 18_command_processor_loop.py
@@ -1,7 +1,11 @@
 # 25. Implement a command processor loop that exits on "quit"/"exit"
 def cmd_proc_loop():
 
-    while (n := int(input("enter the value:" ))) != 'exit':
-        print('you enter the value as:', n)
-    print("program stopped")
+    while True:
+        cmd = input("enter the value:" )
+        if cmd.strip().lower() in ['exit','quit']:
+            print("program stopped")
+            break
+        print('you enter the value as:', cmd)
+    
 cmd_proc_loop()
