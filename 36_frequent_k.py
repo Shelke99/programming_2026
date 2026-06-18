bc30f35b82b5dd7c394e77a0b94e9120030e0ec5
Priyanka Shelke <shelkepriyanka9999@gmail.com>
2026-06-18 18:08:54 +0530

    in integer array nums and an integer k, return the k most frequent elements.

---
+++ 36_frequent_k.py
@@ -0,0 +1,29 @@
+# # Given an integer array nums and an integer k, return the k most frequent elements.
+# freq = {}
+# nums = [1,2,3,4,5,6]
+# for num in nums:
+#     freq[num] = freq.get(num,0) + 1
+# print(freq)
+import heapq
+def freq_k(nums,k):
+    sz = len(nums)
+    freq = {}
+    # nums = [num for num in nums]
+    # heapq.heapify(nums)
+    # print(nums)
+    for num in nums:
+        freq[num] = freq.get(1,0) + 1
+    # print(freq)
+    heap = []
+    for num,count in freq.items():
+        heapq.heappush(heap,(-count, num))
+    
+    ans = []
+    for _ in range(k):
+        count,num = heapq.heappop(heap)
+        ans .append(num)
+    return ans
+
+
+
+print(freq_k([1,1,1,2,3,2,3,1,5,4,1,2],2))
