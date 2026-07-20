8e770fbd0dbf21e6ae18b8ee2503e082b8ccd887
Priyanka Shelke <shelkepriyanka9999@gmail.com>
2026-07-20 18:35:25 +0530

    just car class

---
+++ 78_just_create_class.py
@@ -0,0 +1,10 @@
+class Car:
+    def __init__(self,name):
+        self.name = name
+
+    def display(self):
+        print("the car name is: ",car)
+
+
+Car1 = Car("Maruti_Sujuki")
+print(Car1.name)
