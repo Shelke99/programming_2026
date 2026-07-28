dbfc7fef2d73abd197b045ba66a428954a6b441a
Priyanka Shelke <shelkepriyanka9999@gmail.com>
2026-07-28 18:02:56 +0530

    leetcode find the max profit

---
+++ 85_max_profit.py
@@ -0,0 +1,19 @@
+def maxProfit(prices):
+
+    min_price = prices[0]
+    max_profit = 0
+
+    for price in prices:
+
+        if price < min_price:
+            min_price = price
+
+        profit = price - min_price
+
+        if profit > max_profit:
+            max_profit = profit
+
+    return max_profit
+
+
+print(maxProfit([7,1,5,3,6,4]))
