0607bf54af6a811c5f9276f5b762c3aca87035d0
Priyanka Shelke <shelkepriyanka9999@gmail.com>
2026-08-18 17:23:04 +0530

    create setup in M.Tech lab and write this code

---
+++ 103_read_mark.py
@@ -0,0 +1,57 @@
+# Read marks for five subjects and compute the percentage. Print 
+# (a) Pass/Fail, (b) a grade from an A–F ladder, and 
+# (c) read a grade character and print remarks for it using multi-way selection.
+
+def read_mark():
+    a = float(input("Entert the mark for subject 1: "))
+    b = float(input("Entert the mark for subject 2: "))
+    c= float(input("Entert the mark for subject 3: "))
+    d = float(input("Entert the mark for subject 4: "))
+    e = float(input("Entert the mark for subject 5: "))
+
+    total = a + b + c + d + e 
+    percentage = (total / 500) * 100
+    print(f"Total percentage {percentage:.2f}%")
+
+    if percentage >= 40:
+        status = "Pass"
+    else:
+        status = "Fail"
+    
+
+    if percentage >= 90:
+        grade = "A"
+    elif percentage >= 80:
+        grade = "B"
+    elif percentage >= 70:
+        grade = "C"
+    elif percentage >= 60:
+        grade = "D"
+    elif percentage >= 50:
+        grade = "E"
+    else:
+        grade = "F"
+
+# (c) read a grade character and print remarks for it using multi-way selection.
+    gread = input("Enter the greag: ").upper()
+    if grade == "A":
+        remark = "Excellent performance"
+    elif grade == "B":
+        remark = "Very good job"
+    elif grade == "C":
+        remark = "Good effort, keep imporving"
+    elif grade == "D":
+        remark = "Satisfactory, but needs more work."
+    elif grade == "E":
+        read = "Just passed, requires attention."
+    elif grade == "F":
+        remark = "Failed. Serious improvement needed."
+    else:
+        remark = remark = "Invalid grade character entered."
+    print(f"REMARK {remark}")
+
+
+
+
+read_mark()
+
